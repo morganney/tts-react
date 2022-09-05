8b527eb8dbedf15ad039c5e98dc327a136680b46
morganney <morganney@gmail.com>
2022-09-05 01:03:05 +0000

    Deploying to gh-pages from @ morganney/tts-react@4cfa3e28ceabf1cdf937627b2dd09ae71362a3c4 🚀

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export { TextToSpeech, Positions } from './component'
-export { Sizes } from './icons'
-export { useTts } from './hook'
-
-export type { TTSProps } from './component'
-export type { TTSHookProps, TTSHookResponse, TTSHookState } from './hook'
-export type { TTSBoundaryUpdate, TTSAudioData, PollySpeechMark } from './controller'
