d02fcdc492cc2158acc15c6ab6e4cfe07d5ff5b6
Morgan Ney <morganney@gmail.com>
2024-03-31 17:40:16 -0500

    chore: upgrade to storybook v8. (#72)

--- packages/story/.storybook/main.ts
+++ packages/story/.storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-webpack5'
+import type { TransformOptions } from '@babel/core'
 
 const config: StorybookConfig = {
   stories: ['../src/story.tsx', '../src/count.story.tsx'],
@@ -6,16 +7,14 @@ const config: StorybookConfig = {
     '@storybook/addon-controls',
     '@storybook/addon-actions',
     '@storybook/addon-a11y',
-    '@storybook/addon-docs'
+    '@storybook/addon-docs',
+    '@storybook/addon-webpack5-compiler-babel'
   ],
-  framework: {
-    name: '@storybook/react-webpack5',
-    options : {}
-  },
+  framework: '@storybook/react-webpack5',
   docs: {
     autodocs: 'tag',
   },
-  babel: async (options) => {
+  babel: async (options: TransformOptions) => {
     return {...options, rootMode: 'upward'}
   }
 }
