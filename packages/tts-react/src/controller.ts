063969abd57c5f07e880e4670fa4871b7303322d
Morgan Ney <morganney@gmail.com>
2024-05-05 14:30:03 -0500

    chore: upgrade deploy dep, eslint deps, typescript.

--- packages/tts-react/src/controller.ts
+++ packages/tts-react/src/controller.ts
@@ -386,7 +386,7 @@ class Controller extends EventTarget {
      * @see https://github.com/microsoft/TypeScript-DOM-lib-generator/pull/1300
      */
     if (this.#synthesizer instanceof HTMLAudioElement) {
-      // eslint-disable-next-line @typescript-eslint/no-extra-semi
+      // eslint-disable-next-line no-extra-semi
       ;(
         this.#synthesizer as HTMLAudioElement & { preservesPitch: boolean }
       ).preservesPitch = value
