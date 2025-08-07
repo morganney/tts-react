71aa13c2edb455f7fa8305eb74ab1105f804f0b1
Morgan Ney <morganney@gmail.com>
2025-08-06 21:02:40 -0500

    chore: upgrade storybook.

--- packages/story/.storybook/main.ts
+++ packages/story/.storybook/main.ts
@@ -4,16 +4,11 @@ import type { TransformOptions } from '@babel/core'
 const config: StorybookConfig = {
   stories: ['../src/story.tsx', '../src/count.story.tsx', '../src/update.story.tsx'],
   addons: [
-    '@storybook/addon-controls',
-    '@storybook/addon-actions',
     '@storybook/addon-a11y',
     '@storybook/addon-docs',
     '@storybook/addon-webpack5-compiler-babel'
   ],
   framework: '@storybook/react-webpack5',
-  docs: {
-    autodocs: 'tag',
-  },
   babel: async (options: TransformOptions) => {
     return {...options, rootMode: 'upward'}
   }
